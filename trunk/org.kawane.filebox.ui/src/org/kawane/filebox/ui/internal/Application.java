122c81f217e0f69aeeeb9899ac6e8ef27cd0b45c
jeancharles.roger <jeancharles.roger@594152ae-0296-11de-a43f-ff96a2b7a9af>
2009-02-25 13:01:04 +0000

    Removes @overrides

--- trunk/org.kawane.filebox.ui/src/org/kawane/filebox/ui/internal/Application.java
+++ trunk/org.kawane.filebox.ui/src/org/kawane/filebox/ui/internal/Application.java
@@ -14,7 +14,6 @@ public class Application implements IApplication {
 	
 	private Display display;
 	
-	@Override
 	public Object start(IApplicationContext context) throws Exception {
 		// this the way to retrieve command line option
 		//Object args = context.getArguments().get(IApplicationContext.APPLICATION_ARGS);
@@ -39,11 +38,13 @@ public class Application implements IApplication {
 				display.sleep();
 			}
 		}
+		if(!display.isDisposed()) {
+            display.dispose ();
+		}
 		logger.log(LogService.LOG_INFO, "Stop file box ui");
 		return null;
 	}
 
-	@Override
 	public void stop() {
 		if(display != null && !display.isDisposed()) {
 			display.dispose();
