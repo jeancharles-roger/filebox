eb1c03be936c82f152033a6675c9555df0d0b645
legoff.laurent <legoff.laurent@594152ae-0296-11de-a43f-ff96a2b7a9af>
2009-02-25 09:10:34 +0000

    Share project "org.kawane.filebox.ui" into "https://filebox.googlecode.com/svn"

---
+++ trunk/org.kawane.filebox.ui/src/org/kawane/filebox/ui/internal/Activator.java
@@ -0,0 +1,40 @@
+package org.kawane.filebox.ui.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.log.LogService;
+import org.osgi.util.tracker.ServiceTracker;
+
+public class Activator implements BundleActivator {
+
+	static private Activator instance;
+	private ServiceTracker logTracker;
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		instance = this;
+		logTracker = new ServiceTracker(context, LogService.class.getName(), null);
+		logTracker.open();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		instance = null;
+	}
+	
+	static public Activator getInstance() {
+		return instance;
+	}
+	
+	public LogService getLogger() {
+		LogService logger = (LogService) logTracker.getService();
+		return logger;
+	}
+
+}
