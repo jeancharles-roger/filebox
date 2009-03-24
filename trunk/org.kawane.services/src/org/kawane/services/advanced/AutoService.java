7594b9a845eda37d83fab00afc9b20ca708564b7
legoff.laurent <legoff.laurent@594152ae-0296-11de-a43f-ff96a2b7a9af>
2009-03-24 16:30:26 +0000

    AutoInjection thinking

---
+++ trunk/org.kawane.services/src/org/kawane/services/advanced/AutoService.java
@@ -0,0 +1,9 @@
+package org.kawane.services.advanced;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+public @interface AutoService {
+
+}
