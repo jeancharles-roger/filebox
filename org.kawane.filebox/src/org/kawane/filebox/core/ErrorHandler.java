c6d6718fb9671ee029726d7b05e77d1871fb96c5
jeancharles.roger <jeancharles.roger@594152ae-0296-11de-a43f-ff96a2b7a9af>
2009-07-28 16:04:07 +0000

    Error handler added for boost. some thing adds to be found for filebox error handling, why not this ?

---
+++ org.kawane.filebox/src/org/kawane/filebox/core/ErrorHandler.java
@@ -0,0 +1,39 @@
+/**
+ *  Copyright 2008 GeenSys. All rights reserved.
+ */
+package org.kawane.filebox.core;
+
+/**
+ * <p>
+ * Handles errors for models actions;
+ * </p>
+ * 
+ * @author Jean-Charles Roger 
+ * 
+ */
+public interface ErrorHandler {
+
+	static final int WARNING = 0;
+	static final int ERROR = 1;
+	static final int FATAL_ERROR = 2;
+
+	public void handleError(int type, String message);
+
+	/**
+	 * <p>
+	 * This {@link ErrorHandler} prints errors and warning to console and
+	 * throws a {@link RuntimeException} for ERROR and FATAL_ERROR.
+	 * </p>
+	 */
+	public static final ErrorHandler Stub = new ErrorHandler() {
+		public void handleError(int type, String message) {
+			System.err.println(message);
+			switch (type) {
+			case ERROR:
+			case FATAL_ERROR:
+				throw new RuntimeException(message);
+			}
+		};
+	};
+
+}
