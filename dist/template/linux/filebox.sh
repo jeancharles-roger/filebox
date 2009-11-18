51c4b42e5283cc9219624f8a9821d4c7b1cc3afd
jeancharles.roger <jeancharles.roger@594152ae-0296-11de-a43f-ff96a2b7a9af>
2009-11-18 16:11:50 +0000

    Clean OSGI conf.

--- dist/template/linux/filebox.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-JAVA=java
-OSGI_FRAMEWORK=plugins/org.eclipse.osgi_3.4.0.v20080605-1900.jar
-VMOPTIONS="-Declipse.ignoreApp=true -Dosgi.noShutdown=true -Dosgi.install.area=."
-OPTIONS="-console"
-$JAVA $VMOPTIONS -jar $OSGI_FRAMEWORK $OPTIONS
